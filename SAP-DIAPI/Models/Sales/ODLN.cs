05477a64d48f7bba93b5069b02524a1363cf0132
didierbonilla <106895147+didierbonilla@users.noreply.github.com>
2026-03-13 16:11:39 -0600

    endpoint ventas

---
+++ SAP-DIAPI/Models/Sales/ODLN.cs
@@ -0,0 +1,18 @@
+﻿namespace IntegracionesSAP.Models.Sales
+{
+    public class ODLN
+    {
+        public int? DocNum { get; set; }
+        public string? CardCode { get; set; }
+
+        public int Series { get; set; } = 0;
+
+        public string? Comments { get; set; }
+
+        public int SalesPersonCode { get; set; } = -1;
+
+        public List<UserField> UserFields { get; set; } = new();
+
+        public List<DLN1> Lines { get; set; } = new();
+    }
+}
---
+++ SAP-DIAPI/Models/Sales/OINV.cs
@@ -0,0 +1,17 @@
+﻿namespace IntegracionesSAP.Models.Sales
+{
+    public class OINV
+    {
+        public string? CardCode { get; set; }
+
+        public int? Series { get; set; }
+
+        public string? Comments { get; set; }
+
+        public int SalesPersonCode { get; set; } = -1;
+
+        public List<UserField> UserFields { get; set; } = new();
+
+        public List<INV1> Lines { get; set; } = new();
+    }
+}
