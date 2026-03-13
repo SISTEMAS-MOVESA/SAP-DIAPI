0bcdaa42221786ade69475fc0b34abefae635b80
Roger Jovel <rjovel@gmail.com>
2026-03-13 13:41:09 -0600

    Modificacion factura reserva 13 03 2026

--- SAP-DIAPI/SapDiApi.cs
+++ SAP-DIAPI/SapDiApi.cs
@@ -11,6 +11,7 @@ namespace IntegracionesSAP
         /// </summary>
         public static void ConnectVehico()
         {
+
             if (_company != null && _company.Connected)
                 return;
 
